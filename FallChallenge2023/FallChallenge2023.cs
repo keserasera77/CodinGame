6c37593054f8571c259adf01c2cf424d8784dcbd
勇希 世良 <seraseira.nana@gmail.com>
2024-01-07 15:48:19 +0900

    Main update bronze

--- FallChallenge2023/FallChallenge2023.cs
+++ FallChallenge2023/FallChallenge2023.cs
@@ -191,7 +191,7 @@ class Turn
 /**
  * Score points by scanning valuable fish faster than your opponent.
  **/
-class Main
+class MainClass
 {
     static void Main(string[] args)
     {
@@ -205,12 +205,14 @@ class Main
             int color = int.Parse(inputs[1]);
             int type = int.Parse(inputs[2]);
 
-            fishF.Add(creatureId, color, type);
+            // fishF.Add(creatureId, color, type);
         }
 
+        int turn = 0;
         // game loop
         while (true)
         {
+            turn++;
             int myScore = int.Parse(Console.ReadLine());
             int foeScore = int.Parse(Console.ReadLine());
             int myScanCount = int.Parse(Console.ReadLine());
@@ -226,6 +228,8 @@ class Main
                 int creatureId = int.Parse(Console.ReadLine());
             }
             int myDroneCount = int.Parse(Console.ReadLine());
+            int myBattery = 0, nowX = 0, nowY = 0, myEmergency = 0;
+            var myDrones = new HashSet<int>();
             for (int i = 0; i < myDroneCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
@@ -234,6 +238,11 @@ class Main
                 int droneY = int.Parse(inputs[2]);
                 int emergency = int.Parse(inputs[3]);
                 int battery = int.Parse(inputs[4]);
+
+                nowX = droneX; nowY = droneY;
+                myDrones.Add(droneId);
+                myBattery = battery;
+                myEmergency = emergency;
             }
             int foeDroneCount = int.Parse(Console.ReadLine());
             for (int i = 0; i < foeDroneCount; i++)
@@ -246,14 +255,20 @@ class Main
                 int battery = int.Parse(inputs[4]);
             }
             int droneScanCount = int.Parse(Console.ReadLine());
+            var myScanCnt = 0;
             for (int i = 0; i < droneScanCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int droneId = int.Parse(inputs[0]);
                 int creatureId = int.Parse(inputs[1]);
+                if (myDrones.Contains(droneId))
+                {
+                    creatures.Add(creatureId);
+                    myScanCnt++;
+                }
             }
             int visibleCreatureCount = int.Parse(Console.ReadLine());
-            int x = 5000, y = 5000;
+            int x = 0, y = 0;
             for (int i = 0; i < visibleCreatureCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
@@ -270,20 +285,80 @@ class Main
 
             }
             int radarBlipCount = int.Parse(Console.ReadLine());
+            int tl = 0, tr = 0, bl = 0, br = 0, maxCnt = 0;
             for (int i = 0; i < radarBlipCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
                 int droneId = int.Parse(inputs[0]);
                 int creatureId = int.Parse(inputs[1]);
                 string radar = inputs[2];
+
+                if (creatures.Contains(creatureId))
+                {
+                    continue;
+                }
+                switch (radar)
+                {
+                    case "TL":
+                        {
+                            tl++;
+                            if (tl > maxCnt) maxCnt = tl;
+                            break;
+                        }
+                    case "TR":
+                        {
+                            tr++;
+                            if (tr > maxCnt) maxCnt = tr;
+                            break;
+                        }
+                    case "BL":
+                        {
+                            bl++;
+                            if (bl > maxCnt) maxCnt = bl;
+                            break;
+                        }
+                    case "BR":
+                        {
+                            br++;
+                            if (br > maxCnt) maxCnt = br;
+                            break;
+                        }
+                    default:
+                        break;
+                }
             }
             for (int i = 0; i < myDroneCount; i++)
-
+            {
+                int light = turn % 5 == 0 ? 1 : 0;
+                if (x != 0 && y != 0)
+                {
+                    Console.WriteLine($"Move {x} {y} 0");
+                }
+                else if (myScanCnt > 1 || maxCnt == 0 || myEmergency == 1)
+                {
+                    if (myScanCnt > 0) Console.WriteLine($"MOVE {nowX} 499 0 let's save!");
+                    else Console.WriteLine($"WAIT 0");
+                }
+                else if (br == maxCnt)
+                {
+                    Console.WriteLine($"Move {10000} {10000} {light} {tl} {tr} {bl} {br} battery:{myBattery}");
+                }
+                else if (bl == maxCnt)
+                {
+                    Console.WriteLine($"Move {0} {10000} {light} {tl} {tr} {bl} {br} battery:{myBattery}");
+                }
+                else if (tr == maxCnt)
+                {
+                    Console.WriteLine($"Move {10000} {0} {light} {tl} {tr} {bl} {br} battery:{myBattery}");
+                }
+                else if (tl == maxCnt) Console.WriteLine($"Move 0 0 {light} {tl} {tr} {bl} {br} battery:{myBattery}");
 
                 // Write an action using Console.WriteLine()
                 // To debug: Console.Error.WriteLine("Debug messages...");
 
-                Console.WriteLine($"Move {x} {y} 1 sss"); // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+                // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+
+            }
         }
     }
 }
